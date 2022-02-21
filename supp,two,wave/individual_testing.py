b8d4f334e0edd1b15409a73bb09732706e1e049a
David Hong <dahong67@wharton.upenn.edu>
2022-02-21 15:09:23 -0500

    Add individual testing to LOD,4 and two,wave

---
+++ supp,two,wave/individual_testing.py
@@ -0,0 +1,42 @@
+import numpy as np
+from scipy.sparse import load_npz
+import argparse
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--viral-load-matrix', help='Path to viral load matrix (individuals x time points) in sparse format')
+	parser.add_argument('--savepath', help='Path to save summary figure')
+	parser.add_argument('--start-time', help='Start of time range to analyze',type=int)
+	parser.add_argument('--end-time', help='End of time range to analyze',type=int)
+	parser.add_argument('--LOD', help='Limit of detection',type=float,default=100)
+	args,_ = parser.parse_known_args()
+	# Load data
+	for key,value in vars(args).items():
+		print('%s\t%s' % (key,str(value)))
+	ViralLoad = load_npz(args.viral_load_matrix)
+	print('loaded viral load matrix with shape (%d, %d) and %d entries' % (ViralLoad.shape[0], ViralLoad.shape[1], len(ViralLoad.data)))
+	# Compute prevalence, average efficiency and sensitivity
+	Prevalence = np.zeros(ViralLoad.shape[1])
+	E_avg = np.zeros(ViralLoad.shape[1])
+	Recall_combined = np.zeros(ViralLoad.shape[1])
+	for t in range(args.start_time,args.end_time+1):
+		viralload = ViralLoad[:,t].toarray()[:,0]
+		x_true = (viralload > 0).astype(np.float)
+		x_hat = (viralload > args.LOD).astype(np.float)
+		prev = np.average(viralload > 0)
+		ea = 1.0
+		true_pos = x_true.dot(x_hat)
+		total_pos = x_true.sum()
+		if total_pos > 0:
+			rec = true_pos/total_pos
+		else:
+			rec = 1
+		Prevalence[t] = prev
+		E_avg[t] = ea
+		Recall_combined[t] = rec
+		print('Time: %d; Case frequency: %.5f; Efficiency: %.2f; Sensitivity: %.4f' % (t,prev,ea,rec))
+	np.save('%s/Eff_avg.npy' % (args.savepath),E_avg)
+	np.save('%s/Recall_combined.npy' % (args.savepath),Recall_combined)
+	np.save('%s/Prevalence.npy' % (args.savepath),Prevalence)
+
+# python -u individual_testing.py --viral-load-matrix Simulated_populations_two_wave/seir_viral_loads_swab_switch_SEIR_new_1.viral_loads.npz --savepath designs/individual --start-time 0 --end-time 200 | tee designs/individual/log.txt
